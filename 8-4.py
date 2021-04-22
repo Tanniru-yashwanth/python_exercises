2a496699a4c855d77bfabff0d52ab20de12f816c
iamyash <tanniruyashwanth07@gmail.com>
2021-04-22 16:34:07 +0530

    arg1

---
+++ 8-4.py
@@ -0,0 +1,5 @@
+def make_shirt(size="L", textofamessage="ilovepython"):
+    print(f'the size of shirt is {size},the message is {textofamessage}')
+
+
+make_shirt(size="medium", textofamessage="ilikepython")
