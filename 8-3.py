c482a953185544fdb72f62ff1722b635a4389c8d
iamyash <tanniruyashwanth07@gmail.com>
2021-04-22 16:19:29 +0530

    arguments

---
+++ 8-3.py
@@ -0,0 +1,11 @@
+#def tshirt(size, textofamessage):
+ #   print(f'the size of tshirt is {size},the message is {textofamessage}')
+
+
+#tshirt("L", "16211A04M5")
+
+def tshirt(size, textofamessage):
+    print(f'the size of tshirt is {size},the message is {textofamessage}')
+
+
+tshirt(size="L",textofamessage="16211A04M5")
