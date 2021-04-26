dbfb930da06f7acfb528dcfecafd119b7d52fb6c
iamyash <tanniruyashwanth07@gmail.com>
2021-04-26 09:17:49 +0530

    function1

---
+++ 8-11.py
@@ -0,0 +1,12 @@
+def send_messages(text_message, sent_messages):
+    while text_message:
+        msg = text_message.pop()
+        print(f"text in text_message: {msg} ")
+        sent_messages.append(msg)
+    for text in sent_messages:
+        print(text)
+
+
+msg_1 = ['hi', 'hello', 'gm'[:]]
+sent_message = []
+send_messages(msg_1, sent_message)
