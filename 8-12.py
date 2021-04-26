10bdfe2800035baa567086230dad56be3fc06aee
iamyash <tanniruyashwanth07@gmail.com>
2021-04-26 09:52:12 +0530

    tuples

---
+++ 8-12.py
@@ -0,0 +1,7 @@
+def sandwich(*items):
+    print(f"sandwich ordered with {items}")
+
+
+sandwich('veggies', 'sauces','cheese', 'butter')
+sandwich('chicken', 'sauces')
+sandwich('panner', 'butter', 'cheese')
