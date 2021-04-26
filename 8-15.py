d7643f23a8d578136a1e2962f684a9e4eaaa256b
iamyash <tanniruyashwanth07@gmail.com>
2021-04-26 10:29:43 +0530

    import

---
+++ 8-15.py
@@ -0,0 +1,7 @@
+import printing_functions
+unprinted_design = ['phone case', 'robot pendant', 'dodecahedron']
+completed_model = []
+printing_functions.print_models(unprinted_design, completed_model)
+printing_functions.show_completed_models(completed_model)
+print(unprinted_design)
+print(completed_model)
