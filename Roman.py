f7821a985dfa9c6a6fdccbe5fff59d823e6991f2
Tanniru yashwanth <tanniruyashwanth07@gmail.com>
2021-05-17 19:20:06 +0530

    roman numbers

--- Roman.py
+++ Roman.py
@@ -1,25 +1,24 @@
 class Roman:
-    def __init__(self, integer):
-        self.integer = integer
+    def __init__(self):
         self.numbers = {1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L', 90: 'XC', 100: 'C', 400: 'CD',
                         500: 'D', 900: 'CM', 1000: 'M'}
         self.List = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
 
-    def convert(self):
-        a = ""
+    def convert(self, integer):
+        roman = ""
         for x in self.List:
-            if self.integer != 0:
-                quotient = self.integer / x
+            if integer != 0:
+                quotient = integer / x
 
                 if quotient != 0:
                     for y in range(int(quotient)):
-                        a += self.numbers[x]
-                        a += ""
+                        roman += self.numbers[x]
+                        roman += ""
+                integer = integer % x
+        return roman
 
-                self.integer = self.integer % x
-        return a
 
-
-num = Roman(234)
-b = num.convert()
+num = Roman()
+b = num.convert(234)
 print(b)
+print(num.convert(115))
