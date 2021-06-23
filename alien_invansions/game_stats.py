176b31861226b1dde0bf4c0d1e4ea8033f785ab9
Tanniru yashwanth <tanniruyashwanth07@gmail.com>
2021-06-23 12:00:55 +0530

    pygame_done

--- alien_invansions/game_stats.py
+++ alien_invansions/game_stats.py
@@ -2,7 +2,7 @@ class GameStats:
     def __init__(self, ai_game):
         self.settings = ai_game.settings
         self.reset_stats()
-        self.game_active = True
+        self.game_active = False
         self.high_score = 0
 
     def reset_stats(self):
