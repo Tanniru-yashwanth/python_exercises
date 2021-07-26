3ae1b5bc5df7ca743898ff77a730d6edee0092ff
Tanniru yashwanth <tanniruyashwanth07@gmail.com>
2021-07-26 15:37:29 +0530

    remove_dupliactes_done

---
+++ python_basic_problems/duplicates_lists.py
@@ -0,0 +1,6 @@
+# Python Program to Remove Duplicate Element From a List
+
+list_input = list(input("Enter the list:"))
+"""To remove duplicates from the list we convert into set"""
+list_input = set(list_input)
+print(list_input)
