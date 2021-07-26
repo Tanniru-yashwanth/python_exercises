faed8897aa7fce2512cd8d703ffbcd73f725cad3
Tanniru yashwanth <tanniruyashwanth07@gmail.com>
2021-07-26 11:23:46 +0530

    kilometers_miles_done

---
+++ python_basic_problems/kilometers_miles.py
@@ -0,0 +1,5 @@
+# Python Program to Convert Kilometers to Miles
+
+Kilometers = float(input("Enter the kilometers:"))
+Miles = Kilometers * 0.62137119
+print(f"Conversion from kilometers to miles:{Miles}")
