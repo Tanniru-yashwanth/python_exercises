35b11addcc833ac685a0dcca5334fdf035ce6756
Tanniru yashwanth <tanniruyashwanth07@gmail.com>
2021-07-26 11:28:42 +0530

    leap_year_done

---
+++ python_basic_problems/leap_year.py
@@ -0,0 +1,7 @@
+# Python Program to Check Leap Year
+
+year = int(input("Enter the year:"))
+if year % 4 == 0:
+    print("Given year is leap year")
+else:
+    print("Given year is not a leap year")
