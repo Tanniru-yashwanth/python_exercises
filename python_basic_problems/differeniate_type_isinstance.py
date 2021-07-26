42caec72ed0b05d031afb570cc7be0c705bf0ef9
Tanniru yashwanth <tanniruyashwanth07@gmail.com>
2021-07-26 14:22:49 +0530

    diff_type_isinstance_done

--- python_basic_problems/differeniate_type_isinstance.py
+++ python_basic_problems/differeniate_type_isinstance.py
@@ -2,5 +2,7 @@
 
 input_enter = input("Enter the input:")
 a = type(input_enter)
+"""Type function describes the type of the input i.e, it shows the class"""
 print(a)
+"""isinstance function checks whether the input is the instance of the class"""
 print(isinstance(input_enter, a))
