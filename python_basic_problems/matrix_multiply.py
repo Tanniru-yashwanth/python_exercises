d1383524bef9ba0f565ce0319c419ca766274fc8
Tanniru yashwanth <tanniruyashwanth07@gmail.com>
2021-07-26 17:12:34 +0530

    matrix_multiply_done

---
+++ python_basic_problems/matrix_multiply.py
@@ -0,0 +1,7 @@
+# Python Program to Multiply Two Matrices
+
+import numpy as np
+m1 = np.matrix([[3, 4], [5, 6]])
+m2 = np.matrix([[5, 6], [7, 8]])
+matrix_result = np.multiply(m1, m2)
+print(matrix_result)
