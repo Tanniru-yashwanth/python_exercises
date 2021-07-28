ab6ffd534f7cc927ddcf064a713978d987ca1f5d
Tanniru yashwanth <tanniruyashwanth07@gmail.com>
2021-07-28 10:13:01 +0530

    fib_seq_recursion_done

--- python_basic_problems/fibanocci_recurssion.py
+++ python_basic_problems/fibanocci_recurssion.py
@@ -1,25 +1,12 @@
 # Python Program to Display Fibonacci Sequence Using Recursion
 
 def fib(number):
-    if number == 0:
-        print(0)
-    elif number == 1:
-        print(1)
+    if number <= 1:
+        return number
     else:
-        a = 0
-        b = 1
-        print(a)
-        print(b)
-        for i in range(number):
-            c = a + b
-            print(c)
-            a = b
-            b = c
+        return fib(number - 1) + fib(number - 2)
 
 
-def fiba():
-    number = int(input("Enter the number:"))
-    fib(number)
-
-
-fiba()
+number_in = int(input("Enter the number:"))
+for x in range(number_in):
+    print(fib(x))
